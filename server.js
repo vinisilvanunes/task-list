a1b687452b6a9f27b5d5edb69c9b1bfec23e7818
Vinicius da Silva Nunes <98962398+vinisilvanunes@users.noreply.github.com>
2025-04-04 00:44:16 +0000

    chore(db): conexão no banco de dados

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
+const connectDB = require("./db");
 
 const app = express();
 
 app.use(express.json())
 app.use(cors())
 
+connectDB();
 
 const PORT = process.env.PORT;
 app.listen(PORT, ()=>{
