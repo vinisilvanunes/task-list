e5bbcb6368075fae3549eef2900e3e9ee4e3f26a
Vinicius da Silva Nunes <98962398+vinisilvanunes@users.noreply.github.com>
2025-04-17 22:51:21 +0000

    correção do banco

--- db.js
+++ db.js
@@ -4,7 +4,6 @@ require("dotenv").config();
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
             useUnifiedTopology: true
         });
         console.log("✅ MongoDB Atlas conectado com sucesso!");
